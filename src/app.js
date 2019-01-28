a05a38e670403d7365eec150996e67cd757ee229
Jose Navas <jesquivelnavas@gmail.com>
2019-01-27 18:59:53 -0600

    Add expensify styles

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css'
 import { firebase } from './firebase/firebase'
+import LoadingPage from './components/LoadingPage'
 
 const store = configureStore()
 
@@ -33,7 +34,7 @@ const renderApp = () => {
 }
 
 ReactDOM.render(
-  <p>Loading...</p>,
+  <LoadingPage />,
   document.getElementById('app')
 )
 
